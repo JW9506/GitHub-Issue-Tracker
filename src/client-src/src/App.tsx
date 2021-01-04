70ce895507bda7501a7afb3c788835dfd6bcc21e
JW9506 <cf.dota@gmail.com>
2021-01-04 02:58:28 -0500

    listen for keys update

--- src/client-src/src/App.tsx
+++ src/client-src/src/App.tsx
@@ -1,27 +1,56 @@
 import { useEffect, useState } from 'react';
 import { Http } from '#/utils/Http';
-import { Main, Left, Right, Body, MessageBox } from '#/containers';
+import { Main, Body } from '#/containers';
+import { Switch, Route, useHistory } from 'react-router-dom';
 import { Header } from '#/components';
-import { Text } from './containers/Text';
+import WelcomePage from './pages/WelcomePage';
+import Issues from './pages/Issues';
+import { KeyToLink } from '#/config';
+import Repositories from './pages/Repositories';
+import PullRequests from './pages/PullRequests';
+import { useSelector } from 'react-redux';
 
 function App() {
-  const [data, setData] = useState('');
+  const [, setData] = useState('');
+  const history = useHistory();
   useEffect(() => {
     (async () => {
       const { data: xhrData } = await Http<any>({ url: '123' });
       setData(JSON.stringify(xhrData));
     })();
   }, []);
+  const { current } = useSelector((st) => st.focus);
+
+  useEffect(() => {
+    const handleNavKey = (e: KeyboardEvent) => {
+      if (!KeyToLink[e.key]) {
+        return;
+      }
+      if (
+        !KeyToLink[e.key]['when'] ||
+        KeyToLink[e.key]['when']!.some((item) =>
+          Object.keys(current).includes(item)
+        )
+      ) {
+        history.push(KeyToLink[e.key].link);
+      }
+    };
+    document.addEventListener('keydown', handleNavKey);
+    return () => {
+      document.removeEventListener('keydown', handleNavKey);
+    };
+  }, [current]);
+
   return (
     <Main>
       <Header />
       <Body>
-        <MessageBox>
-          <Text center>Welcome to Github Manager</Text>
-          <Text>Name: Foobar</Text>
-          <Text>Bio: C/C++ Javascript/Typescript Python Golang ~</Text>
-          <Text>{data}</Text>
-        </MessageBox>
+        <Switch>
+          <Route exact path="/" component={WelcomePage} />
+          <Route path="/issues" component={Issues} />
+          <Route path="/repositories" component={Repositories} />
+          <Route path="/pull-requests" component={PullRequests} />
+        </Switch>
       </Body>
     </Main>
   );
